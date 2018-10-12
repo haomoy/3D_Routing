4e4954ee95402d2e224be0321c4ff6efc337f27c
haomoy <36680159+haomoy@users.noreply.github.com>
2018-10-12 12:37:40 -0600

    Added GUI buttons for help and browsing

--- 3D_Routing.py
+++ 3D_Routing.py
@@ -2,24 +2,205 @@
 #Description-
 
 import adsk.core, adsk.fusion, adsk.cam, traceback
+from .File_Extraction import file_browser as Browse         
+#from .UTEPGuiModules import Reference1 as R       
+#from .UTEPGuiModules import ComponentList as CL   
+handlers = []
+global selectedFiles
+selectedFiles = []
+global directories
+directories = []
+global constructionPlanes
+constructionPlanes = []
+global selectedPlanes
+selectedPlanes = []
+
+app = None
+ui  = None
+commandId = 'CommandInputGallery'
+commandName = 'UTEP Fusion 360 GUI'
+commandDescription = 'Demo command input examples.'
+rowNumber = 0
+
+# Global set of event handlers to keep them referenced for the duration of the command
+handlers = []
+
+
+# Event handler for the execute event.
+class ButtonPressedHandler(adsk.core.InputChangedEventHandler):
+    def __init__(self):
+        super().__init__()
+    def notify(self, args):
+        eventArgs = adsk.core.InputChangedEventArgs.cast(args)
+
+        # Code to react to the event.
+        app = adsk.core.Application.get()
+        ui  = app.userInterface
+        try:
+            cmdInput = eventArgs.input
+            inputs = eventArgs.inputs
+          
+            global buttonClicked
+            buttonClicked = ""
+        
+            #selectInput = selectionArgs.id
+            #ui.messageBox("SelectionEvent: "+str(selectInput))
+            
+            if cmdInput.id == '_help':
+                ui.messageBox('''
+Tab Overview: 
+This Add-Inn allows you to select an EAGLE 360 schematic file and translate their components into Fusion 360 Sketches.  
+
+Browse: 
+This button opens File Explorer, allowing you to select the EAGLE schematic file to be translated.
+After file(s) are selected, a new tab will appear next to "UTEP Gui". 
+This tab also contains a Help button to guide you through the software. 
+
+Create Reference: 
+This button creates a shape to be used as geometric reference on the printing bed.
+If unsure of what this means you can press it, it will not interfere or modify your current work.
+''')
+                
+            if cmdInput.id == '_reference':
+                buttonClicked = "_reference"
+                action = cmd.doExecute(False)
+                buttonClicked = ""
+        
+            if cmdInput.id == '_browse':
+                temp = B.run(contextt)
+                files = temp[0]
+                
+                for j in range(0, len(files)):#for every selected file, get the file name and append it to list
+                    tempName = files[j]
+                    indexOfLastSlash = 0
+                    for i in range(0, len(tempName)):
+                        if(tempName[i] == "/"):
+                            indexOfLastSlash = i+1        
+                    filename = tempName[indexOfLastSlash:]
+                    directories.append(tempName[0:indexOfLastSlash])
+                    selectedFiles.append(filename)
+                buttonClicked = '_browse'
+                action = cmd.doExecute(False)
+                buttonClicked = ""
+            
+        except:
+            if ui:
+                ui.messageBox('Failed:\n{}'.format(traceback.format_exc()))	
+
+class MyCommandDestroyHandler(adsk.core.CommandEventHandler):
+    def __init__(self):
+        super().__init__()
+    def notify(self, args):
+        try:
+            # When the command is done, terminate the script
+            # This will release all globals which will remove all event handlers
+            adsk.terminate()
+        except:
+            if ui:
+                ui.messageBox('Failed:\n{}'.format(traceback.format_exc()))
+
+class MyCommandCreatedHandler(adsk.core.CommandCreatedEventHandler):
+    def __init__(self):
+        super().__init__()
+    def notify(self, args):
+        try:
+            app = adsk.core.Application.get()
+            ui = app.userInterface
+            global cmd
+            cmd = args.command
+            cmd.isOKButtonVisible = False
+            global inputss
+            inputss = cmd.commandInputs
+            global commandId
+            commandId = 'ComponentGallery'
+            # Create tab input 1
+            tabCmdInput1 = inputss.addTabCommandInput(commandId + '_table_1', 'UTEP Gui')
+            global tab1ChildInputs            
+            tab1ChildInputs = tabCmdInput1.children
+            
+            
+            #help
+            tab1ChildInputs.addBoolValueInput('_help', 'Help', False, '', True)
+            # Create bool value input with button style
+            tab1ChildInputs.addBoolValueInput('_browse', 'Browse', False, '', True)
+            # Create bool value input with button style # reference
+            tab1ChildInputs.addBoolValueInput('_reference', 'Create Reference', False, '', True)
+            
+                        
+            # Connect to the execute event.
+            onExecute = CommandExecuteHandler()
+            cmd.execute.add(onExecute)
+            handlers.append(onExecute)
+            
+            onInputChanged = ButtonPressedHandler()
+            cmd.inputChanged.add(onInputChanged)
+            handlers.append(onInputChanged)
+            
+        except:
+            if ui:
+                ui.messageBox('Failed:\n{}'.format(traceback.format_exc()))
+class CommandExecuteHandler(adsk.core.CommandEventHandler):
+        def __init__(self):
+            super().__init__()
+        def notify(self, args):
+            try:
+                app = adsk.core.Application.get()
+                ui = app.userInterface
+                #ui.messageBox("inside execute handler")
+                command = args.firingEvent.sender
+                if buttonClicked == '_browse':
+                    #ui.messageBox("inside if browse statement")
+                    CL.run(contextt, selectedFiles, directories)
+                    #COWS.run(contextt, inputss, selectedFiles)
+                if buttonClicked == "_reference":
+                    R.CreateReference2()
+                #if buttonClicked == "_place":
+                #    P.PlaceComponents(contextt, )
+                
+            except:
+                if ui:
+                    ui.messageBox(('command executed failed: {}').format(traceback.format_exc()))
 
 def run(context):
     ui = None
     try:
+        global app
         app = adsk.core.Application.get()
-        ui  = app.userInterface
-        ui.messageBox('Hello addin')
+        global ui
+        ui = app.userInterface
+        global contextt
+        contextt = context
 
-    except:
-        if ui:
-            ui.messageBox('Failed:\n{}'.format(traceback.format_exc()))
+        global commandId
+        global commandName
+        global commandDescription
 
-def stop(context):
-    ui = None
-    try:
-        app = adsk.core.Application.get()
-        ui  = app.userInterface
-        ui.messageBox('Stop addin')
+        # Create command defintion
+        cmdDef = ui.commandDefinitions.itemById(commandId)
+        if not cmdDef:
+            cmdDef = ui.commandDefinitions.addButtonDefinition(commandId, commandName, commandDescription)
+        
+        #R.run(contextt)
+        # Add command created event
+        onCommandCreated = MyCommandCreatedHandler()
+        cmdDef.commandCreated.add(onCommandCreated)
+        # Keep the handler referenced beyond this function
+        handlers.append(onCommandCreated)
+
+        # Execute command
+        cmdDef.execute()
+
+        # Prevent this module from being terminate when the script returns, because we are waiting for event handlers to fire
+        #docu = app.activeDocument
+        #datafolder = adsk.core.DataFolder
+        #ui.messageBox("Has this file been saved before? "+str(docu.isSaved))
+        #docu.saveAs("hello", datafolder.parentFolder,"","")
+        
+        #if(saved == True):
+        #    ui.messageBox("File was successfully saved!!!")
+        #if(saved == False):
+        #    ui.messageBox("File failed to save.")
+        adsk.autoTerminate(False)
 
     except:
         if ui:
---
+++ component_collector.py
@@ -0,0 +1,99 @@
+
+import adsk.core, adsk.fusion, adsk.cam
+from xml.etree import ElementTree as ET
+from copy import deepcopy
+global app
+global ui
+app = adsk.core.Application.get()
+ui = app.userInterface
+
+def run(directory, filename):
+    
+    directory_name = directory
+    directory_name = directory_name + filename[:len(filename)-4] + "_output_XML.xml"
+    e = ET.parse(directory_name)
+
+    components = {}
+    nets = {}
+    pin_length_reference = {'short': 2.54, 'middle': 5.08, 'long': 7.62}
+    
+    for component in e.iter('Component'):
+        component_name = component.get('name')
+        height  = component.get('height')
+        
+        Instance = component[0]
+        Perimeter = component[1]
+        Pins = component[2]
+        
+        crosshair = (Instance.get('x'), Instance.get('y'))
+        
+        cnt_value = component.get('cnt') if component.get('cnt') else 0
+        component_rot = Instance.get('rot') if Instance.get('rot') else "R0"
+        
+        component_coordinates = {'x':[], 'y':[]}
+        
+        for wire in Perimeter:
+            component_coordinates['x'].append(wire.get('x1'))  
+            component_coordinates['x'].append(wire.get('x2'))  
+            component_coordinates['y'].append(wire.get('y1'))  
+            component_coordinates['y'].append(wire.get('y2'))  
+        
+      
+        component_pins = {}
+        for pin in Pins:
+            pin_name = str(pin.get('name'))
+            pin_ID = component_name + "_" + pin_name
+            gauge = pin.get('gauge')
+            pin_coordinates = (pin.get('x'), pin.get('y'))
+            pin_rot = pin.get('rot') if pin.get('rot') else "R0"
+            pin_length = pin_length_reference[pin.get('length')]
+            
+            component_pins[pin_name] = {'ID': pin_ID, 'gauge': gauge, 'coordinates': pin_coordinates, 
+                          'rot': pin_rot, 'length': pin_length, 'component': component_name}
+            
+            
+        components[component_name] = {'height ': height , 'crosshair': crosshair, 'cnt': cnt_value, 'rot': component_rot,
+                  'coordinates': component_coordinates, 'pins': component_pins} 
+        
+    '''   
+    for name_component, component_characteristic in components.items(): 
+        print(" ")
+        print("###################################")
+        print("Component name: " + name_component)
+        print("     height : " + str(component_characteristic["height "]))
+        print("     CrossHair: " + str(component_characteristic["crosshair"]))  
+        print("     CNT: " + str(component_characteristic["cnt"]))  
+        print("     Rot: " + str(component_characteristic["rot"]))
+        print("     Coordinates: " + str(component_characteristic["coordinates"]))  
+        print("     Pins: ")  
+        for name_pin, pin_characteristics in component_characteristic['pins'].items():
+            print("          Pin name: " + name_pin)  
+            print("          ID: " + str(pin_characteristics["ID"]))  
+            print("          gauge: " + str(pin_characteristics["gauge"]))  
+            print("          coordinates: " + str(pin_characteristics["coordinates"]))
+            print("          rot: " + str(pin_characteristics["rot"]))
+            print("          length: " + str(pin_characteristics["length"]))
+            print(" ")
+    '''
+     
+    for net in e.iter('Net'):
+        net_name = net.get('name')
+        print("##################################")
+        print("Current net: " + net_name)
+        current_net_pins = []
+        
+        for pin in net.iter('Pin'):
+            pin_component = pin.get('component')
+            if pin_component not in ['VCC', 'GND']: 
+                pin_name = str(pin.get('name'))
+                print("     Pin component: " + pin_component)
+                print("     Pin name: " + pin_name)
+                current_net_pins.append(deepcopy(components[pin_component]['pins'][pin_name]))
+            
+        nets[net_name] = current_net_pins
+        
+   
+    ui.messageBox("Returning from component_collector")
+    
+    
+    return nets, components 
